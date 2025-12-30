f1f1e7ad9c627c3cb29f1818089b45ddaa3e6a27
Sahil pratap <sahilpratapsingh9924@gmail.com>
2025-12-30 20:31:57 +0530

    Update import path for authMiddleware to .mjs

--- server/routes/leave.mjs
+++ server/routes/leave.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import verifyUser from "../middleware/authMiddleware.js";
+import verifyUser from "../middleware/authMiddleware.mjs";
 import {
   addLeave,
   getLeave,
